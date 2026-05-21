4f23716099926a3ddd206b281da1e04317d7dc7a
DaniOK123 <daniellopresti@icloud.com>
2026-05-21 22:26:36 +0200

    funzione delete creata

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 
+print('Welcome everyone to my list of task. \nThis is your list right now.')
+
 with open("task.json", "r") as f:  
     dati = json.load(f) 
  
@@ -7,30 +9,49 @@ for elemento in dati["studio"]:
     print(elemento["materia"]) 
 
 
-
-print('Welcome everyone to my list of task.')
-
-
-
 def overwiev():
     print('Click 1 to add something to your task')
+    print('Click 2 to delete something to your task')
+
+    decision = int(input('What you wanna do: '))
+    return decision
 
 
+def add(decision):
 
-def add():
-    add1 = int(input('What you wanna do: '))
-
-    if add1 == 1:
-        nuova_materia = input('cosa vuoi aggiungere: ')
-        dati["studio"].append({"materia" : nuova_materia})
+    if decision == 1:
+        nuova_attività = input('cosa vuoi aggiungere: ')
+        dati["studio"].append({"materia" : nuova_attività})
 
         with open("task.json", "w") as f:
-            json.dump(dati, f, indent = 4)
+            json.dump(dati, f, indent = 4, ensure_ascii = False)
 
-        print(f'Nuova materia {nuova_materia} aggiunta!')
+        print(f'Nuova materia {nuova_attività} aggiunta!')
+
+
+def delete(decision):
+    
+    if decision == 2:
+        elimina_attività = input('Che attività vuoi eliminare dalla lista? ')
+        
+        # cerca e rimuove l'elemento
+        for elemento in dati["studio"]:
+            if elemento["materia"] == elimina_attività:
+                dati["studio"].remove(elemento)
+                
+                with open("task.json", "w") as f:
+                    json.dump(dati, f, indent=4, ensure_ascii=False)
+                
+                print(f'Materia {elimina_attività} eliminata!')
+                return  # esce dalla funzione dopo aver eliminato
+        
+        # se non trova nulla
+        print(f'Attività {elimina_attività} non trovata nella lista!')
 
 
 
-overwiev()
-add()
 
+
+scelta = overwiev()
+add(scelta)
+delete(scelta)
